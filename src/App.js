06ba8b0c0d96face7e7e35e8e23daa8f6f534d82
Gian <giansingh4710@gmail.com>
2021-06-21 12:59:09 -0400

    sant ji khata

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { khata } from "./khata";
 
 function App() {
+  const [message, setMessage] = useState("....No Khatas Yet");
+  const [ang, setAng] = useState("");
+
+  function khataForAng(angNum) {
+    function randId() {
+      return Math.random().toString(36).substr(2, 9);
+    }
+    function whatToReturn() {
+      let a;
+      try {
+        a = khata[angNum].map((i) => {
+          return (
+            <li key={randId()}>
+              <a href={i} target="_blank">
+                {i}
+              </a>
+              <video controls name="media">
+                <source src={i} type="audio/mpeg" />
+              </video>
+            </li>
+          );
+        });
+      } catch (e) {
+        a = ".....No Khatas for this ang";
+        console.log(e);
+      }
+      return a;
+    }
+    return <ol>{whatToReturn()}</ol>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div>Khata of Sant Giani Gurbachan Singh Ji Bhindran Wale</div>
+      <div>Enter the Ang Number for which you want khata of:</div>
+      <div>
+        <input
+          type="number"
+          placeholder="ex: 1084"
+          value={ang}
+          onChange={(event) => {
+            setAng(event.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            if (0 < ang && ang < 1431) {
+              console.log(ang);
+              setMessage(() => {
+                let theKhata = khataForAng(ang);
+                console.log(theKhata);
+                return theKhata ? theKhata : "....No Khatas Yet";
+              });
+            } else {
+              alert("Please enter vallid ang number");
+            }
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Submit
+        </button>
+        <div>{message}</div>
+      </div>
     </div>
   );
 }
